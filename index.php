6effe3658ba7c2de1572dbc18bc069855aa68691
Start Coding Student 2 <session2@START-Coding-MBP-25.local>
2016-09-30 18:10:48 -0700

    sdsdf

--- index.php
+++ index.php
@@ -172,3 +172,35 @@ if(@$post['submit']){
         crossorigin="anonymous"></script>
 </body>
 </html>
+
+CREATE TABLE `blog_post_tags` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`blog_id` int(11) NOT NULL,
+`tag_id` int(11) NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `blogs` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`title` text NOT NULL,
+`body` text NOT NULL,
+`author_id` int(11) NOT NULL,
+`createdate` date NOT NULL,
+`url` varchar(255) DEFAULT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `peoples` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`name` varchar(255) NOT NULL,
+`email` varchar(255) DEFAULT NULL,
+`joindate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `tags` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`name` varchar(255) NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
